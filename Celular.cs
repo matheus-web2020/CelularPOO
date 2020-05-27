e9418250106e6748be4c49ab84d816d4feeb7dd2
Matheus <matheus.fusca.lima@outlook.com>
2020-05-27 16:29:04 -0300

    iniciado

---
+++ Celular.cs
@@ -0,0 +1,18 @@
+namespace CelularPOO
+{
+    //Atributos
+    public class Celular
+    {
+       public string cor;
+
+       public string modelo;
+
+        public float tamanho;
+
+        public bool ligado; 
+
+        // Métodos
+        
+
+    }
+}
